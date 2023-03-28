2ab557eebc583e3831ef7a11b9815a1b833cc96c
Imran <eigoooinc@Eigooos-MacBook-Pro.local>
2023-03-29 04:55:29 +0600

    Final update for ChatGPT-3.5 booster with working code.

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 import requests
 from django.http import JsonResponse
 from django.shortcuts import render
+import openai
 
 
 def process_text(text):
@@ -11,22 +15,31 @@ def chat_page(request):
     return render(request, 'chat.html')
 
 
+def getting_ready(message):
+    load_dotenv()
+    openai.api_key = os.getenv('API_KEY')
+    openai.organization = os.getenv('ORG_KEY')
+
+    completion = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return str(completion.choices[0].text)
+
+
 def chat(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = requests.post('https://api.openai.com/v1/engines/curie/completions', json={
-            "prompt": message,
-            "max_tokens": 100,
-            "temperature": 0.5
-        }, headers={
-            'Authorization': 'Bearer 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'
-        })
-        print(response.content)
-        if response.ok:
-            print(response.content)
-            response_text = response.json()['choices'][0]['text']
-            response_text = process_text(message + response_text)
+        response = getting_ready(message)
+        # print(response)
+        if response:
+            # print(response)
+            response_text = response
         else:
             response_text = "Oops, something went wrong"
-        print(response_text)
+        # print(response_text)
         return JsonResponse({'response_text': response_text})
